b52b7564f78dec943512134bd2c7400e5cae2e26
lschons917 <lorenzodebrumschons@gmail.com>
2026-09-18 12:06:01 -0300

    Adiciona metodos de Concessionaria

--- Concessionaria.java
+++ Concessionaria.java
@@ -7,4 +7,28 @@ public class Concessionaria {
         this.name = name;
         this.address = address;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
