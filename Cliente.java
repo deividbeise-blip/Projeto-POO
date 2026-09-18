09d51b4e45938d6d2d9eed6393d2e4d57047f88d
lschons917 <lorenzodebrumschons@gmail.com>
2026-09-18 12:05:18 -0300

    Adiciona metodos de Cliente

--- Cliente.java
+++ Cliente.java
@@ -11,4 +11,44 @@ public class Cliente {
         this.email = email;
         this.phone = phone;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
