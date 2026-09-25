3743e15fb7ebf0c3905c14882b9a4d391c212b01
matheus fonsesca <matheusfdo.aluno@unipampa.edu.br>
2026-09-25 15:46:45 -0300

    adiciona a interface gráfica da tela pincipal

--- interfacegrafica/TelaPrincipal.java
+++ interfacegrafica/TelaPrincipal.java
@@ -10,19 +10,36 @@ public class TelaPrincipal extends JFrame {
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //fechar tela no X
         setLocationRelativeTo(null); //qual posaeção a tela vai aparecer
-    }
 
-    //criação das abas
-    JTabbedPane abas = new JTabbedPane();
 
-    //painéis
-    JPanel painelClientes =  new JPanel();
-    JPanel painelVendas = new JPanel();
-    JPanel painelVeiculos = new JPanel();
+        //criação das abas
+        JTabbedPane abas = new JTabbedPane();
 
-    //Conteudo temporario
+        //painéis
+        JPanel painelClientes = new JPanel();
+        JPanel painelVendas = new JPanel();
+        JPanel painelVeiculos = new JPanel();
+
+        //Conteudo temporario
         painelClientes.add(new JLabel("Cadastro de Clientes"));
         painelVeiculos.add(new JLabel("Cadastro de Veículos"));
         painelVendas.add(new JLabel("Realização de Vendas"));
 
+        //Adicionando os painéis às abas
+        abas.addTab("Clientes", painelClientes);
+        abas.addTab("Veículos", painelVeiculos);
+        abas.addTab("Vendas", painelVendas);
+
+        //Adicionando as abas pra janela
+        add(abas);
+    }
+
+    //comando para ativar a execução da tela
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            TelaPrincipal tela = new TelaPrincipal();
+            tela.setVisible(true);
+        });
+    }
 }
